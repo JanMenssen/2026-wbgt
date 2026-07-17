2710408ade7045b585eddf6b3402a72fed36de95
janmenssen <j.menssen@me.com>
2026-07-17 15:01:55 +0200

    tsest

--- python/test.py
+++ python/test.py
@@ -20,18 +20,45 @@ class OpenWBGT :
         
         print("in init")
 
+    # input
+	  #
+	  #   asks the user for start and stop dates
+
     def input(self) :
         
-        print("in input") 
+      print("in input") 
+
+    # setLocation
+	  #
+	  #		sets the location for which the data should be retrieved	
+
+    def setLocation(self) :
+            
+      print(" in seocation")    
+
+    # readData
+    #
+    #		reads tthe data from the KNMI sites
+    #			- first read a list of CSV files that should be processed
+    #			- process each file
+    #		at the end the wbgtTable is filled
 
     def readData(self) :
         
-        print("in readData")  
+      print("in readData")  
 
+    # plot
+    #
+    #		plots the data in the wbgtTable
+    
     def plot(self) :
         
-        print("in plot")
+      print("in plot")
   
+  	# list
+    #
+    #		lists the data in the wbgtTable	
+
     def list(self) :
         
-        print("in list")  
+      print("in list")  
